0cbf94f39283e1f800ade3aa692ce7ba9ed372ee
rangelZZZ <18792561236@163.com>
2017-08-15 11:06:28 +0800

    finished todo-redux-react TodoList component and add wpw to package.json

--- public/js/index.js
+++ public/js/index.js
@@ -7,7 +7,7 @@ class App extends React.Component {
     render() {
         return <div>
             <AddTodo/>
-            {/*<TodoList/>*/}
+            <TodoList/>
             {/*<Footer/>*/}
         </div>;
     }
@@ -19,6 +19,23 @@ class AddTodo extends React.Component {
         return <div>
             <input type="text"/>
             <button type="butoon">添加</button>
+        </div>
+    }
+}
+
+class TodoList extends React.Component {
+
+    render() {
+        let todoList = [1, 2, 3].map((element,index) => {
+            return <div key={index}>
+                <input type="checkbox"/>
+                <span>{element}</span>
+                <button type="button">删除</button>
+            </div>
+        });
+
+        return <div>
+            {todoList}
         </div>
     }
 }
