7239d9cabc851a356ea9cb2ade8d2a35c5936329
rangelZZZ <18792561236@163.com>
2017-08-15 10:50:58 +0800

    add AddTodo component

--- public/js/index.js
+++ public/js/index.js
@@ -3,12 +3,25 @@ import {Component} from "react-dom";
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-    render(){
+
+    render() {
         return <div>
-            hello world
+            <AddTodo/>
+            {/*<TodoList/>*/}
+            {/*<Footer/>*/}
         </div>;
     }
-};
+}
+
+
+class AddTodo extends React.Component {
+    render() {
+        return <div>
+            <input type="text"/>
+            <button type="butoon">添加</button>
+        </div>
+    }
+}
 
 ReactDOM.render(<App/>, document.getElementById("root"));
 
